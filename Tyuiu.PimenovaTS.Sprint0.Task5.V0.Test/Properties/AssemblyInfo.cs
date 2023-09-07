b5f7c7e062dac1edd0fe104fb3a4644864453fc4
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-09-07 16:20:07 +0500

    Task5

---
+++ Tyuiu.PimenovaTS.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PimenovaTS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PimenovaTS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26fec42c-c0d4-46c7-9733-a0e4b1eb9685")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
